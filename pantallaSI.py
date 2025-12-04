d9b6fd04e3958cd493041f401c82d588412f5003
Derek2195-git <derek.garzon@uabc.edu.mx>
2025-12-03 17:50:23 -0800

    Añadido el movimiento de los enemigos

--- pantallaSI.py
+++ pantallaSI.py
@@ -1,29 +1,29 @@
 import pygame
-import test_bridge
+import enemigos
 
-from ctypes import WinDLL, c_int
-
-from test_bridge import moverJugador
+from ctypes import WinDLL, c_int, byref
+from test_bridge import moverJugador, definirValor, actualizarPosicionEnemigos, init_enemigos, update_all_enemigos, \
+    get_enemigo_data
+try:
+    dll = WinDLL(r"C:\Users\Keraf\source\repos\PuentePrueba\Debug\PuentePrueba.dll")
+except Exception as e:
+    print("DLL no encontrada")
+    dll = None
 
 # Creación de la ventana aqui
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load("Expedition33_GestralBeach.mp3")
-pygame.mixer.music.play(-1)
-WIDTH = 100
-HEIGHT = 100
+#pygame.mixer.music.play(-1)
+WIDTH = 400
+HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Invaders - La prueba")
 
-clock = pygame.time.Clock()
-FPS = 60
-
-# Cargamos el DLL aqui
-try:
-    dll = WinDLL(r"C:\Users\Keraf\source\repos\PuentePrueba\Debug\PuentePrueba.dll")
-except Exception as e:
-    print("DLL no encontrada, prueba solo gráfica")
-    dll = None
+player_img = pygame.image.load("naveprincipal.png").convert_alpha()
+enemy_img = pygame.image.load("naveprincipal.png").convert_alpha()
+player_img = pygame.transform.scale(player_img, (50, 50))
+enemy_img = pygame.transform.scale(enemy_img, (50, 50))
 
 # Datos rapidos para el jugador
 player_x = WIDTH // 2
@@ -32,11 +32,18 @@ player_width = 50
 player_height = 50
 player_speed = 3
 
-# Cargar imagen de la nave
-player_img = pygame.image.load("naveprincipal.png").convert_alpha()
+clock = pygame.time.Clock()
+FPS = 60
 
-# (Opcional) Redimensionar si está muy grande
-player_img = pygame.transform.scale(player_img, (player_width, player_height))
+# Cargamos el DLL aqui
+
+
+
+
+if dll:
+    init_enemigos(2,5)
+enemigo = enemigos.Enemigos(10, 50, 1, enemy_img)
+enemigo2 = enemigos.Enemigos(40, 50, 5, enemy_img)
 
 
 # Bucle de juego?
@@ -45,7 +52,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    # Movimiento del jugador
     keys = pygame.key.get_pressed()
     direccion = 0
     if keys[pygame.K_LEFT]:
@@ -53,17 +60,30 @@ while running:
     if keys[pygame.K_RIGHT]:
         direccion = 1
 
-    player_x = moverJugador(player_x, direccion, player_speed)
+    if dll:
+        player_x = moverJugador(player_x, direccion, player_speed)
 
+    # Limitar jugador a pantalla
     if player_x < 0:
         player_x = 0
-    if player_x + player_width > WIDTH:
-        player_x = WIDTH - player_width
+    if player_x > WIDTH - 50:
+        player_x = WIDTH - 50
+    # Actualizar todos los enemigos (en MASM)
+    if dll:
+        update_all_enemigos(WIDTH)
 
+    # Dibujar
     screen.fill((0, 0, 0))
-    # Jugador
-    #pygame.draw.rect(screen, (40, 222, 85), (player_x, player_y, player_width, player_height))
     screen.blit(player_img, (player_x, player_y))
+    if dll:
+        for i in range(55):  # 5*11 = 55 enemigos
+            x = c_int()
+            y = c_int()
+            alive = c_int()
+
+            if get_enemigo_data(i, byref(x), byref(y), byref(alive)):
+                if alive.value:
+                    screen.blit(enemy_img, (x.value, y.value))
 
     pygame.display.flip()
     clock.tick(FPS)
--- test_bridge.py
+++ test_bridge.py
@@ -1,4 +1,5 @@
-from ctypes import WinDLL, c_int
+import ctypes
+from ctypes import WinDLL, c_int, c_void_p, POINTER
 
 dll = WinDLL(r"C:\Users\Keraf\source\repos\PuentePrueba\Debug\PuentePrueba.dll")
 
@@ -6,6 +7,28 @@ prueba = dll.pruebaPuente
 prueba.argtypes = (c_int, c_int)
 prueba.restype = c_int
 moverJugador = dll.moverJugadorAsm
+definirValor = dll.definirValorAsm
+actualizarPosicionEnemigos = dll.actualizarPosicionEnemigosAsm
+actualizarPosicionEnemigos.argtypes = [
+    POINTER(c_int),  # pos_x (puntero a int)
+    POINTER(c_int),  # pos_y (puntero a int)
+    c_int,           # velocidad
+    POINTER(c_int),  # dir (puntero a int)
+    c_int            # ancho
+]
+
+init_enemigos = dll.initEnemigos
+init_enemigos.argtypes = [c_int, c_int]
+init_enemigos.restype = None
+
+update_all_enemigos = dll.updateAllEnemigos
+update_all_enemigos.argtypes = [c_int]
+update_all_enemigos.restype = None
+
+get_enemigo_data = dll.getEnemigoData
+get_enemigo_data.argtypes = [c_int, POINTER(c_int), POINTER(c_int), POINTER(c_int)]
+get_enemigo_data.restype = c_int
+
 
 if (prueba(5,20)==25):
     prueba_completa = "cargado."
